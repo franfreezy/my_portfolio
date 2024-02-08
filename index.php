fb518eabe66536a8ff8934119e4f34477cd309e1
franfreezy <frandelwanjawa@yahoo.com>
2024-02-08 13:49:09 +0300

    additional

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-
+//this is the login page
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PORTFOLIO HOME</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
+   
 </body>
 </html>
