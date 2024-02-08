bae8c989056e4482bb596876db7a56074f6b955b
franfreezy <frandelwanjawa@yahoo.com>
2024-02-08 13:51:29 +0300

    additional

---
+++ registration.php
@@ -0,0 +1,12 @@
+// registration page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRATION PAGE</title>
+</head>
+<body>
+    
+</body>
+</html>
