519b3d2c40e22ed2a51e550122421a9329fd387f
franfreezy <frandelwanjawa@yahoo.com>
2024-02-08 13:53:27 +0300

    additional

---
+++ blog.php
@@ -0,0 +1,12 @@
+//this page is the plog of life and times of one code with freezy
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOGS</title>
+</head>
+<body>
+    
+</body>
+</html>
